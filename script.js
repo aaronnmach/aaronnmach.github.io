294ac04427ed0ac8a5f52777b9fc68b496018937
amach8 <98847891+amach8@users.noreply.github.com>
2024-02-22 02:55:00 -0800

    reworked entire website

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// script.js
-
 // Function to change the background color of the header when clicked
 function changeHeaderColor() {
     var header = document.querySelector('header');
@@ -19,3 +17,24 @@ function toggleFooterVisibility() {
     var footer = document.querySelector('footer');
     footer.style.display = (footer.style.display === 'none') ? 'block' : 'none';
 }
+
+function toggleSectionVisibility(sectionId) {
+    var sections = document.querySelectorAll('section'); // Get all sections
+    sections.forEach(function (section) {
+        if (section.id === sectionId) { // Show the clicked section
+            section.style.display = 'block';
+        } else { // Hide other sections
+            section.style.display = 'none';
+        }
+    });
+}
+document.addEventListener('DOMContentLoaded', function () {
+    // Hide all sections except the Home section when the page loads
+    var sections = document.querySelectorAll('section');
+    sections.forEach(function (section) {
+        if (section.id !== 'home-section') {
+            section.style.display = 'none';
+        }
+    });
+});
+
