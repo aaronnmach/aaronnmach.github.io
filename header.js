53fe28b8a99d7cca2ce3d5512ed2f43e82529d26
amach8 <98847891+amach8@users.noreply.github.com>
2024-03-03 05:55:46 -0800

    Add files via upload

---
+++ header.js
@@ -0,0 +1,14 @@
+// header.js
+document.write(`
+<header>
+        <div>
+            <img src="images/aaronnmach1.png" alt="Image Description" class="circular-border">
+        </div>
+        <div>
+            <h1 style="color: #A29592">Aaronn Mach</h1>
+        </div>
+        <div class="container">
+            <h3 id="header-text" style="color: #A29592"></h3>
+        </div>
+    </header>
+`);
